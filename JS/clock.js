f5967401bbf1e0f775cb98fbc02ce22663d6458b
Hwang08 <pmhwang8@gmail.com>
2019-12-24 11:12:16 -0500

    Change logic for clock format to 1-12 and add am pm functionality

--- JS/clock.js
+++ JS/clock.js
@@ -3,11 +3,18 @@ const clockContainer = document.querySelector('.js-clock'),
 
 function getTime() {
   const date = new Date(); //instantiate Date objet and set it to a constant variable
-  const seconds = date.getSeconds(); // Gets the date objects seconds value into a const
+  // const seconds = date.getSeconds(); // Gets the date objects seconds value into a const // PASTE THIS CODE IN FOR SECONDS TO SHOW :${seconds < 10 ? `0${seconds}` : seconds} 
   const minutes = date.getMinutes();
-  const hours = (date.getHours() + 11) % 12 +1;
+  const hours = date.getHours();
+  const suffix = (hours >= 12) ? 'pm' : 'am';
+
+  // If hours is greater than 12 subtract 12 from military time to equal format 1-12
+  (hours > 12) ? hours - 12 : hours;
+  // If hours is equal to 0(military time) or midnight return 12 for standard time format for midnight
+  (hours === 0) ? 12 : hours;
+
   // Ternary operator (conditional statement ? "if code block" : "else code block")
-  clockTitle.innerText = `${hours < 10 ? `0${hours}` : hours}:${minutes < 10 ? `0${minutes}` : minutes}:${seconds < 10 ? `0${seconds}` : seconds}`;
+  clockTitle.innerText = `${hours < 10 ? `0${hours}` : hours}:${minutes < 10 ? `0${minutes}` : minutes} ${suffix}`;
 }
 
 // initialize application
